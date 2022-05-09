5eb06e3808cb04cd1c23d9aa76e67d2de6eeeaee
vsp <github.com/vishnusureshperumbavoor>
2022-05-09 16:04:19 +0530

    username in header. logout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React,{useEffect,useContext} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Signup from './Pages/Signup'
+import Login from './Pages/Login'
 import './App.css';
-
-/**
- * ?  =====Import Components=====
- */
 import Home from './Pages/Home';
+import { AuthContext, FirebaseContext } from './store/Context';
 
 function App() {
+  const {setUser} = useContext(AuthContext)
+  const {firebase} = useContext(FirebaseContext)
+  useEffect(()=>{
+    firebase.auth().onAuthStateChanged((user)=>{
+      setUser(user)
+    })
+  })
   return (
     <div>
       <Router>
@@ -17,6 +22,9 @@ function App() {
         </Route>
         <Route path="/signup">
           <Signup />
+        </Route>
+        <Route path="/login">
+          <Login/>
         </Route>
       </Router>
     </div>
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,9 +1,11 @@
 import React,{useState,useContext} from 'react';
-import { FirebaseContext } from '../../store/FirebaseContext';
+import { FirebaseContext } from '../../store/Context';
+import { useHistory } from 'react-router-dom';
 import Logo from '../../olx-logo.png';
 import './Signup.css';
 
 export default function Signup() {
+  const history = useHistory()
   const [username,setUsername] = useState('')
   const [email,setEmail] = useState('')
   const [phone,setPhone] = useState('')
@@ -12,7 +14,16 @@ export default function Signup() {
   const handleSubmit=(e)=>{
     e.preventDefault()
     firebase.auth().createUserWithEmailAndPassword(email,password).then((result)=>{
-      result.user.updateProfile({displayName:username})
+      result.user.updateProfile({displayName:username}).then(()=>{
+        firebase.firestore().collection('users').add({
+          id:result.user.uid,
+          username:username,
+          email:email,
+          phone:phone
+        }).then(()=>{
+          history.push("/login")
+        })
+      })
     })
   }
   return (
