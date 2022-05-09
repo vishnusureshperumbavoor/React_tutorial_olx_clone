a91f540eb0a1da440754a534a21cadeb82ca04ab
vsp <github.com/vishnusureshperumbavoor>
2022-05-09 13:43:03 +0530

    signup page

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,14 +1,21 @@
-import React from 'react';
-
+import React,{useState} from 'react';
 import Logo from '../../olx-logo.png';
 import './Signup.css';
 
 export default function Signup() {
+  const [username,setUsername] = useState('')
+  const [email,setEmail] = useState('')
+  const [phone,setPhone] = useState('')
+  const [password,setPassword] = useState('')
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log(username);
+  }
   return (
     <div>
       <div className="signupParentDiv">
         <img width="200px" height="200px" src={Logo}></img>
-        <form>
+        <form onSubmit = {handleSubmit}>
           <label htmlFor="fname">Username</label>
           <br />
           <input
@@ -17,6 +24,10 @@ export default function Signup() {
             id="fname"
             name="name"
             defaultValue="John"
+            value={username}
+            onChange={(e)=>
+              setUsername(e.target.value)
+            }
           />
           <br />
           <label htmlFor="fname">Email</label>
@@ -27,6 +38,10 @@ export default function Signup() {
             id="fname"
             name="email"
             defaultValue="John"
+            value={email}
+            onChange={(e)=>
+              setEmail(e.target.value)
+            }
           />
           <br />
           <label htmlFor="lname">Phone</label>
@@ -37,6 +52,10 @@ export default function Signup() {
             id="lname"
             name="phone"
             defaultValue="Doe"
+            value={phone}
+            onChange={(e)=>
+              setPhone(e.target.value)
+            }
           />
           <br />
           <label htmlFor="lname">Password</label>
@@ -47,6 +66,10 @@ export default function Signup() {
             id="lname"
             name="password"
             defaultValue="Doe"
+            value={password}
+            onChange={(e)=>
+              setPassword(e.target.value)
+            }
           />
           <br />
           <br />
